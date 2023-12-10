1a4424b8e03a048bb20afadaed75a9fff8dd8c80
dveselka <daniel.veselka@gmail.com>
2023-12-10 15:18:15 +0100

    add sample Weblogic 12.2.1.3 domain in volume

--- docker-domain/build.sh
+++ docker-domain/build.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
-if [ "$#" -eq 0 ]; then echo "Provide a password for the domain as first argument."; exit; fi
-docker build --build-arg ADMIN_PASSWORD=$1  -t 1213-domain . 
+#! /bin/bash
+#
+#Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+#
+#Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+#Build WebLogic Domain image persisted to volume
 
+docker build -f Dockerfile -t 12213-weblogic-domain-in-volume .
